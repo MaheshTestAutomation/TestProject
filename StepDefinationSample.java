ff4cf3ce63dc1b9cf299251adfbb8fbd52bbf5e9
MaheshTestAutomation <53488629+MaheshTestAutomation@users.noreply.github.com>
2020-02-17 02:15:02 +0530

    Update StepDefinationSample.java

--- StepDefinationSample.java
+++ StepDefinationSample.java
@@ -1,6 +1,6 @@
 //1st sheet data
 	@Then("^I validate list of properties in page zero with data in excel at \"([^\"]*)\"$")
-	public void i_validate_list_of_dynamic_tag_properties_in_page_zero_with_data_in_excel_at(@Transform(ExcelDataToDataTable.class)  DataTable table) throws Throwable {
+	public void i_validate_list_of_properties_in_page_zero_with_data_in_excel_at(@Transform(ExcelDataToDataTable.class)  DataTable table) throws Throwable {
 	System.out.println(table.toString());
     List<String> dataList=table.asList(String.class);
 
